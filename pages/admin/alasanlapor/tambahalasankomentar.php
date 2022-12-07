b3f89a4d71658f6e5ff2fad7b78de8282d5f22ad
mahdanz <ahmadmahdan5@gmail.com>
2022-12-07 22:31:43 +0700

    first commit

---
+++ pages/admin/alasanlapor/tambahalasankomentar.php
@@ -0,0 +1,15 @@
+<?php
+
+include('../../../koneksi.php');
+
+$judul_alasan =  htmlspecialchars($_POST['judul_alasan']);
+$deskripsi =  htmlspecialchars($_POST['deskripsi']);
+$targetalasan = $_POST['targetalasan'];
+ 
+echo $judul_alasan;
+// menginput data ke database
+mysqli_query($koneksi,"INSERT into tb_alasanlapor values('','$judul_alasan','$deskripsi','$targetalasan')");
+
+
+header("location:komentar.php");
+?>
